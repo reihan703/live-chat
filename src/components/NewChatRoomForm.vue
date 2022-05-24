1d4af4075371a723b02bf09316387967c23b3f3a
Reihan <reihanyasfinu@gmail.com>
2022-05-24 13:32:59 +0700

    save chat to firebase

---
+++ src/components/NewChatRoomForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <textarea 
+  placeholder="Insert text" 
+  v-model="message" 
+  @keypress.enter.prevent="handleSubmit"
+  >
+  </textarea>
+  <div class="error">{{ error }}</div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+import getUser from '@/composables/getUser'
+import useCollection from '@/composables/useCollection'
+import { timestamp } from '../firebase/config'
+
+export default {
+  setup(){
+    const message = ref('')
+    const { user } = getUser()
+    const {error, addDoc} = useCollection('messages')
+
+    const handleSubmit = async () => {
+      const chat = {
+        message: message.value,
+        name: user.value.displayName,
+        createdAt: timestamp(),
+        uid: user.value.uid
+      }
+
+      await addDoc(chat)
+      message.value = ('')
+    }
+
+    return { message, handleSubmit, error }
+  }
+}
+</script>
+
+<style scoped>
+  form{
+    margin: 10px;
+  }
+
+  textarea{
+    width: 100%;
+    max-width: 100%;
+    margin-bottom: 6px;
+    padding: 20px;
+    box-sizing: border-box;
+    border: 0;
+    border-radius: 20px;
+    font-family: inherit;
+    outline: none;
+  }
+</style>
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="container">
     <Navbar />
+    <NewChatRoomForm />
   </div>
 </template>
 
 <script>
+import NewChatRoomForm from '../components/NewChatRoomForm.vue'
 import Navbar from '../components/Navbar.vue'
 import getUser from '../composables/getUser'
 import { useRouter } from 'vue-router'
 import { watch } from '@vue/runtime-core'
 export default {
-  components:{ Navbar },
+  components:{ Navbar, NewChatRoomForm },
   setup(){
     const { user } = getUser()
     const router = useRouter()
